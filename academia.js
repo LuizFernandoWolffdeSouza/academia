7fafd5af378bd7ce11f2258761f71b40ab3f3250
LuizFernandoWolffdeSouza <Wolfluid@gmail.com>
2024-06-27 13:25:28 -0300

    aqui vai

--- academia.js
+++ academia.js
@@ -1,24 +1,27 @@
+const { removeListener } = require("process")
 const readline = require("readline")
 const rl = readline.createInterface({
-   input: process.stdin,
-   output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 })
 
 
 menu()
 let alunos = []
+let treino = []
 
 
 function menu() {
     console.log(`
     --------Menu GYM --------
     1.Fazer matrícula
-    2.alterar inform
-    3.listar alunos cadastrados
+    2.Editar matrícula
+    3.Listar alunos cadastrados
     4.Excluir aluno
-    5.editar,adicionar planos de treino
-    6.registrar frequência
-    7.sair`)
+    5.adicionar planos de treino
+    6.Editar plano de treino
+    7.Registrar frequência
+    8.Sair`)
 
     rl.question('Escolha uma opção', (resposta) => {
         switch (resposta) {
@@ -38,9 +41,13 @@ function menu() {
                 planotreino()
                 break;
             case '6':
-                registrarfrequencia()
+                editartreino()
                 break;
             case '7':
+                registrarfrequencia()
+
+                break;
+            case '8':
                 rl.close()
 
                 break;
@@ -56,14 +63,6 @@ function menu() {
 
 //1-Mátricula
 function fazermatricula() {
-    let abc = {
-        segunda: "Peito e tríceps",
-        terça: "Costas e bíceps",
-        quarta: "Perna completo",
-        quinta: "Peito e tríceps",
-        sexta: "Costas e bíceps",
-        sábado: "Perna completa"
-    }
 
     rl.question('Digite seu nome: ', (nome) => {
         rl.question('Digite seu cpf: ', (matricula) => {
@@ -77,6 +76,35 @@ function fazermatricula() {
         })
     })
 }
+//2- Editar cadastro
+function alterarinfo() {
+    if (vetor.length == 0) {
+        console.log("Nenhum cadastro foi concedido")
+    } else {
+        rl.question('Digite o número do elemento que deseja editar: ', (elemento) => {
+            if (elemento > 0 && elemento <= alunos.length) {
+                rl.question('Digite a nova propriedade 1: ', (nome) => {
+                    rl.question('Digite a nova propriedade 2: ', (cpf) => {
+                        rl.question('Digite a nova propriedade 3: ', (idade) => {
+                            alunos[numero - 1] = {
+                                nome,
+                                cpf,
+                                idade
+                            }
+                            console.log('editado com sucesso!')
+                            exibirMenu()
+                        })
+                    })
+                })
+            } else {
+                console.log('Número inválido, tente novamente.')
+                exibirMenu()
+            }
+        })
+
+    }
+
+}
 //3-Listar alunos
 function listaraluno() {
     if (alunos.lenght == 0) {
@@ -84,11 +112,8 @@ function listaraluno() {
     }
     else {
         for (let i = 0; i < alunos.length; i++) {
-            console.log(`A lista de alunos cadastrados é
-            ${alunos[i].nome}
-            ${alunos[i].matricula} 
-            ${alunos[i].idade}`)
-          console.log(alunos)
+            console.log("A lista de alunos cadastrados é")
+            console.log(alunos)
         }
     }
     menu()
@@ -96,11 +121,132 @@ function listaraluno() {
 }
 //4-Excluir alunos
 function excluiraluno() {
-    if (alunos.length == ) {
-        
+    if (alunos.lenght == 0) {
+        console.log("Nenhum cadastrado sucedido")
+        menu()
     }
-    
+    else {
+        for (let i = 0; i < alunos.length; i++) {
+            rl.question("Digite o número do aluno que deseja remover: ", (remover) => {
+                if (remover >= 0 && remover <= alunos.length) {
+                    alunos.splice(remover - 1, 1)
+                    console.log("aluno removido com sucesso")
+                    menu()
+
+                }
+                else {
+                    console.log("Número inválido, digite novamente.")
+                    menu()
+                }
+
+            })
+
+
+        }
+    }
+
 }
+// 5 - adicionar plano de treino
+function planotreino() {
+    let abc = {
+        Segunda: "Peito, ombro e tríceps",
+        Terça: "Costas e bíceps",
+        Quarta: "Perna completa",
+        Quinta: "Peito, ombro e tríceps",
+        Sexta: "Costas e bíceps",
+        Sábado: "Perna completa"
+    }
+    let abc2 = {
+        Segunda: "Peito, ombro e tríceps",
+        Terça: "Costas e bíceps",
+        Quarta: "Perna completa"
+    }
+    let abcd = {
+        Segunda: "Peito e tríceps",
+        Terça: "Costas e bíceps",
+        Quarta: "Perna completa",
+        Quinta: "Ombro e trápezio"
+
+    }
+
+    let abcde = {
+        Segunda: "Peito ",
+        Terça: "Costas ",
+        Quarta: "Perna completa",
+        Quinta: "Ombro",
+        Sexta: "Braço"
+
+    }
+    console.log(abc)
+    console.log(abc2)
+    console.log(abcd)
+    console.log(abcde)
+    rl.question("Quantos dias da semana você consegue ir? (3 dias é o minimo): ", (dias) => {
+        switch (dias) {
+            case '3':
+                treino.push(abc2)
+                console.log(`Esse treino é o ideal treino  ${treino}`)
+                menu()
+            case '4':
+                treino.push(abcd)
+                console.log(`Esse treino é o ideal treino  ${treino}`)
+                menu()
+
+                break;
+            case '5':
+                treino.push(abcde)
+                console.log(`Esse treino é o ideal treino  ${treino}`)
+                menu()
+
+            case '6':
+                treino.push(abc)
+                console.log(`Esse treino é o ideal treino  ${treino}`)
+                menu()
+
+            default:
+                console.log("Nenhum treino foi escolhido")
+                menu()
+                break;
+        }
+
+
+    })
+}
+
+//6 - editar plano de treino
+function editar() {
+    if (treino.length == 0) {
+     console.log("Nenhum treino cadastrado")
+    } else {
+        rl.question('Digite o seu treino que deseja editar: ', (numero) => {
+            if (numero > 0 && numero <= treino.length) {
+                rl.question('Digite a nova propriedade 1: ', (propriedade1) => {
+                    rl.question('Digite a nova propriedade 2: ', (propriedade2) => {
+                        rl.question('Digite a nova propriedade 3: ', (propriedade3) => {
+                            vetor[numero - 1] = {
+                                propriedade1,
+                                propriedade2,
+                                propriedade3
+                            }
+                            console.log('editado com sucesso!')
+                            exibirMenu()
+                        })
+                    })
+                })
+            } else {
+                console.log('Número inválido, tente novamente.')
+                exibirMenu()
+            }
+        })
+
+    }
+}
+
+//7-editar treino
+function Editartreino(params) {
+
+}
+
 
 
 
---
+++ readme.md
@@ -0,0 +1,34 @@
+# GYM all day
+ Este é um sistema de gestão de academia, o qual permite `Matricular`, `editar`, `listar`, `excluir`, `gerenciar planos de treino` e `registrar frequência`
+ ## Estruturação do código
+
+ ### Desenvolvimento
+
+ - `readline`: É um módulo utilizado para determinar a entrada e saída
+ - `rl` utiliza-se para ler a codificação  e ler a entrada
+ - dentro do `rl` utilizamos o ` input e output` para determinar a entrada e saída com `process.stdin  e stdout `
+
+ ### Funções principais
+
+ - `fazermatricula()` : realiza a matricula 
+ - `alterarinfo()` : permite alterar os dados da matrícula
+ -  `Listaraluno()` : permite listar os alunos cadastrados
+ - `excluiraluno()` : possibilita excluir os dados de um aluno cadastrado
+ - `planodetreino()` : disponibiliza escolher um treino de acordo com a possibilidade da pessoa
+ - `editar treino()` : permite ao aluno editar o treino se ele preferir
+ - `registrarfrequencia()`: registra a frequencia semanal da pessoa em porcentagem
+
+ ### Códificação
+ - `Variáveis` utilizadas for `alunos e push`.
+ - se utilizou vetores `arrays` para manipular os dados recebido.
+ - Mutuamente com os `arrays`, se utilizou algumas funções como `push()` e `splice()`.
+ - Se utilizou as condicionais `if else` e `switch case`.
+ - foi utilizado somente o laço de repetição `for`.
+
+ - A `INTERFACE`, é bem básica, possibilitando uma fácil compreensão e manipulação.
+
+
+
+
+
+
